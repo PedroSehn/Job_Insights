6e7fe49d19ba5713e439e61e91106415bca7762d
PedroSehn <pedrorsehn@hotmail.com>
2023-01-23 18:02:12 -0300

    fix: ex3

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -6,7 +6,7 @@ def get_unique_industries(path: str) -> List[str]:
     data = read(path)
     unique_industries = []
     for job in data:
-        job_industry = job['industry']
+        job_industry = job["industry"]
         if job_industry not in unique_industries:
             unique_industries.append(job_industry)
     return unique_industries
