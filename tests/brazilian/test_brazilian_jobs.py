ea95d4b42ebe44110871ce0490c92376ef5c454f
PedroSehn <pedrorsehn@hotmail.com>
2023-02-03 23:41:18 -0300

    trabalho pronto

--- tests/brazilian/test_brazilian_jobs.py
+++
@@ -1,7 +0,0 @@
-from src.pre_built.brazilian_jobs import read_brazilian_file
-
-
-def test_brazilian_jobs():
-    result = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
-    first_value = {"title": "Maquinista", "salary": '2000', "type": "trainee"}
-    assert result[0] == first_value
--- tests/counter/test_counter.py
+++
@@ -1,8 +0,0 @@
-
-from src.pre_built.counter import count_ocurrences
-
-
-def test_counte():
-    'Deve retornar 1639'
-    result = count_ocurrences('data/jobs.csv', 'Python')
-    assert result == 1639
